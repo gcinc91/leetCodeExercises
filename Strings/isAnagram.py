b41b6392857b669d15862f7de5267623e4814f3e
Gabriel Cueva <gcinc@Gabriels-MacBook-Pro.local>
2026-04-08 16:53:31 +0200

    Upload isAnagram exercise

---
+++ Strings/isAnagram.py
@@ -0,0 +1,38 @@
+'''
+
+Given two strings, determine whether they are anagrams.
+
+Any word that exactly reproduces the letters in another order is an anagram. In other words, X and Y are anagrams if by rearranging the letters of X, we can get Y using all the original letters of X exactly once.
+
+Input: X = 'silent', Y = 'listen'
+Output: True
+
+Input: X = 'incest', Y = 'insect'
+Output: True
+
+'''
+
+class Solution:
+	def isAnagram(self, X: str, Y: str) -> bool:
+		# Write your code here...
+		if len(X) != len(Y): 
+			return False
+			
+		X_table = {}
+		Y_table = {}
+		
+		for i in range(len(X)):
+			print('i ', i)
+			if X_table.get(X[i], False):
+				X_table[X[i]] += 1
+			else:
+				X_table[X[i]] = 1
+				
+			if Y_table.get(Y[i], False):
+				Y_table[Y[i]] += 1
+			else:
+				Y_table[Y[i]] = 1
+				
+		return X_table == Y_table
+				
+
