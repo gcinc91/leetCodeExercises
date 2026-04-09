118665ac004ac1c9cf2ce6dab75008d2bf1547cb
Gabriel Cueva <gcinc@Gabriels-MacBook-Pro.local>
2026-04-10 01:28:20 +0200

    Upload mergeTwotLikendList exercise

---
+++ LinkedList/mergeTwoLists.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    if (!list1 && !list2) return null;
+    if (list1 && !list2) return list1;
+    if (list2 && !list1) return list2;
+
+    let resultList = new ListNode();
+    let curr = resultList;
+
+    while (list1 != null && list2 != null) {
+        if (list1.val < list2.val) {
+            curr.next = list1
+            list1 = list1.next
+        } else {
+            curr.next = list2
+            list2 = list2.next
+        }
+        curr = curr.next;
+    }
+
+    curr.next = list1 !== null ? list1 : list2;
+
+    return resultList.next;
+};
